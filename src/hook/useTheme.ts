a110f6949590e538c76b02bc21c5f2385466ae98
kauan777 <kauan7622@gmail.com>
2023-01-05 16:41:34 -0300

    style: prettier

--- src/hook/useTheme.ts
+++ src/hook/useTheme.ts
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-import { ThemeContext } from "../context/ThemeContext";
+import { ThemeContext } from "../contexts/ThemeContext";
 
-
-export const useTheme = () => useContext(ThemeContext)
+export const useTheme = () => useContext(ThemeContext);
